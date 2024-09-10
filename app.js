c81d397da633f9a20a6ac8b86467a1c9056325fc
Abdalla Alhag <alhagabdalla@gmail.com>
2024-09-10 09:08:24 -0700

    Update port

--- app.js
+++ app.js
@@ -56,6 +56,5 @@ app.use('/', authRouter);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
-const HOST = '0.0.0.0';
-app.listen(PORT, HOST, () => console.log("app listening on port 3000!"));
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", () => console.log("app listening on port 8080!"));
